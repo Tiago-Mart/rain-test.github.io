7915781b05b04b12da5a84517b14edb1689db4a9
Tiago Martins <82526424+Tiago-Mart@users.noreply.github.com>
2021-12-08 13:39:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    /*$('input.special-button').click(function(event){
+        $(this).addClass('teste')
+        setTimeout(function(){
+            $(event.target).css("animation-play-state", "paused");
+        }, 250);
+    })/*
+    
+    /*     $("input.teste").css("animation-play-state", "paused");
+
+    $('input.teste').click(function(event){
+        $(event.target).css("animation-play-state", "running");
+        
+
+        setTimeout(function(){
+            $(event.target).css("animation-play-state", "paused");
+        }, 2);
+    }) */
+})
