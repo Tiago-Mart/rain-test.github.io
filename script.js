e71f692023f7919bbdaa3b18b232c8ef8539d816
Tiago Martins <82526424+Tiago-Mart@users.noreply.github.com>
2021-12-08 13:30:18 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('div.item-card').click(function(event){
-        var target = $(this);
-        $(this).addClass('view1');
-        $(this).removeClass('view2');
-
-        $('div.geral').css('height', '100%')
-    })
-})
